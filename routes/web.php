d419cab52f3195b59c76bf72c30f31d01e13c3c5
ayana1709 <ayanabashachali@gmail.com>
2024-04-05 23:41:56 +0300

    Routing with the tasks

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "main page";
+});
+Route::get('/xxx', function(){
+    return "Hello";
+})->name("hello");
+Route:: get('/hallo', function(){
+    return redirect()->route('hello');
+});
+Route::get('/greet/{name}', function($name){
+    return 'hello '. $name . "!";
+});
+Route::fallback(function(){
+    return'still got somewhere';
 });
